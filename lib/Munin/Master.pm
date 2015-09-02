84328a2359f64ce80380ac8fb702c85b6ffca9f5
Stig Sandbeck Mathisen <ssm@fnord.no>
2015-09-02 23:52:31 +0200

    Move initialization to subs

--- lib/Munin/Master.pm
+++ lib/Munin/Master.pm
@@ -11,6 +11,25 @@ sub startup {
     # Documentation browser under "/perldoc"
     $self->plugin('PODRenderer');
 
+    $self->_setup_config;
+    $self->_setup_static;
+    $self->_setup_routes;
+    $self->_setup_helpers;
+}
+
+sub _setup_config {
+    my $self   = shift;
+    my $config = $self->plugin('Config');
+}
+
+sub _setup_static {
+    my $self = shift;
+    $self->static->paths->[0] = $self->home->rel_dir('web/static');
+}
+
+sub _setup_routes {
+    my $self = shift;
+
     # Router
     my $r = $self->routes;
 
@@ -24,22 +43,16 @@ sub startup {
 
     # Simple test routes
     $r->get('/graph/test/')->to('graph#test');
+}
 
-    # Where to serve static files from
-    $self->static->paths->[0] = $self->home->rel_dir('web/static');
-
-    my $config = $self->plugin('Config');
+sub _setup_helpers {
+    my $self = shift;
 
     $self->helper(
         'db' => sub {
             my $dbfile = $self->config->{dbfile};
-            state $dbh = DBI->connect(
-                "dbi:SQLite:$dbfile",
-                undef, undef,
-                {
-                    sqlite_open_flags => DBD::SQLite::OPEN_READONLY,
-                }
-            );
+            state $dbh = DBI->connect( "dbi:SQLite:$dbfile", undef, undef,
+                { sqlite_open_flags => DBD::SQLite::OPEN_READONLY, } );
         }
     );
 }
