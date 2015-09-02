d67f2e8b755cdf74573ce183166c9b80f37d20bd
Stig Sandbeck Mathisen <ssm@fnord.no>
2015-09-02 18:39:31 +0200

    perltidy

--- lib/Munin/Master.pm
+++ lib/Munin/Master.pm
@@ -3,27 +3,28 @@ use Mojo::Base 'Mojolicious';
 
 # This method will run once at server start
 sub startup {
-  my $self = shift;
+    my $self = shift;
 
-  # Documentation browser under "/perldoc"
-  $self->plugin('PODRenderer');
+    # Documentation browser under "/perldoc"
+    $self->plugin('PODRenderer');
 
-  # Router
-  my $r = $self->routes;
+    # Router
+    my $r = $self->routes;
 
-  # Normal route to controller
-  $r->get('/')->to('HTML#welcome');
-  $r->get('/node')->to('HTML#node');
-  $r->get('/service')->to('HTML#service');
-  $r->get('/comparison')->to('HTML#comparison');
-  $r->get('/group')->to('HTML#group');
-  $r->get('/problems')->to('HTML#problems');
+    # Normal route to controller
+    $r->get('/')->to('HTML#welcome');
+    $r->get('/node')->to('HTML#node');
+    $r->get('/service')->to('HTML#service');
+    $r->get('/comparison')->to('HTML#comparison');
+    $r->get('/group')->to('HTML#group');
+    $r->get('/problems')->to('HTML#problems');
 
-  # Simple test routes
-  $r->get('/graph/test/')->to('graph#test');
+    # Simple test routes
+    $r->get('/graph/test/')->to('graph#test');
 
-  # Switch to installable "public" directory
-  $self->static->paths->[0] = $self->home->rel_dir('web/static');
+    # Switch to installable "public" directory
+    $self->static->paths->[0] = $self->home->rel_dir('web/static');
+
 
 }
 
