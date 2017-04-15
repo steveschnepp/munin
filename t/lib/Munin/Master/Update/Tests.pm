dea877cd76148ff75b3eb139a2e78acd49788843
Steve Schnepp <steve.schnepp@pwkf.org>
2017-04-15 10:58:28 +0200

    t: enhancing the test for Update.pm

--- t/lib/Munin/Master/Update/Tests.pm
+++ t/lib/Munin/Master/Update/Tests.pm
@@ -1,7 +1,7 @@
 package Munin::Master::Update::Tests;
 use base qw(Test::Class);
 use Test::More;
-use Test::MockObject;
+use Test::MockModule;
 
 use File::Temp qw(tempdir);
 
@@ -13,7 +13,14 @@ sub setup : Test(setup) {
 
     $self->{update} = Munin::Master::Update->new();
 
+    my %log;
+    $log{output} = 'screen';
+    $log{level}  = 'debug';
+
+    Munin::Common::Logger::configure(%log);
+
     $self->{config} = Munin::Master::Config->instance()->{config};
+    $self->{config}->parse_config_from_file();
 
 }
 
@@ -22,7 +29,7 @@ sub class : Test(1) {
     isa_ok( $update, 'Munin::Master::Update' );
 }
 
-sub method__run : Test(2) {
+sub method__run : Test(3) {
     my $self   = shift;
     my $update = $self->{update};
 
@@ -32,21 +39,19 @@ sub method__run : Test(2) {
       . ' and provide a munin node on localhost:4949'
       unless $ENV{TEST_HEAVY};
 
-    $self->{mock} = Test::MockObject->new();
-    $self->{mock}->fake_module(
-        'RRDs',
-        'create' => sub(@) { },
-        'error'  => sub { },
-    );
+    my $mock_rrds = Test::MockModule->new("RRDs");
+    $mock_rrds->mock('create',  sub(@) { });
+    $mock_rrds->mock('error',  sub { });
+
+    $mock_node = Test::MockModule->new("Munin::Master::Node");
+    $mock_node->mock('do_in_session', sub { my ($self, $block) = @_; return { exit_value => $block->() }; });
 
     my $config = Munin::Master::Config->instance()->{config};
     $config->{dbdir}  = tempdir( CLEANUP => 1 );
     $config->{rundir} = tempdir( CLEANUP => 1 );
 
-  TODO: {
-        local $TODO = 'Not implemented yet';
-        ok( eval{$update->run} );
-    }
+    ok( eval{$update->get_dbh()} );
+    ok( eval{$update->run} );
 }
 
 1;
